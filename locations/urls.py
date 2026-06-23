c5d51ce8f0cd41c221f72109239492c85fa7faa6
Dmitrii Polovinkin <dtpolovinkin@gmail.com>
2026-06-23 16:07:53 +0700

    basic urls and views files

---
+++ locations/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = "locations"
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("<slug:country_slug>/", views.country_detail, name="country_detail"),
+    path("<slug:country_slug>/<slug:city_slug>/", views.city_detail, name="city_detail"),
+]
--- locations/views.py
+++ locations/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Country, City, Location
 
-# Create your views here.
+def home(request):
+    countries = Country.objects.all().order_by("name")
+    return render(request, "locations/home.html", {"countries": countries})
+
+def country_detail(request, country_slug):
+    country = get_object_or_404(Country, slug=country_slug)
+    cities = City.objects.filter(country=country).order_by("name")
+    return render(request, "locations/country_detail.html", {"country": country, "cities": cities})
+
+def city_detail(request, country_slug, city_slug):
+    country = get_object_or_404(Country, slug=country_slug)
+    city = get_object_or_404(City, slug=city_slug, country=country)
+    locations = Location.objects.filter(city=city)
+    return render(request, "locations/city_detail.html", {"city": city, "locations": locations})
